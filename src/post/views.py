edc8449cd83be84021f30bcad1c66ce6924a653a
cvargas2019289 <cvargas-2019289@kinal.edu.gt>
2021-09-23 17:12:21 -0600

    creacion de las vistas

--- src/post/views.py
+++ src/post/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from post.models import Post, PostView, Like, Comment
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'autor',
+        'slug',
+    )
+
+
+class PostDeleteView(DeleteView):
+    model = Post
