6408aa2bc63d5d5e3f8b082b2842c405610d477f
cvargas2019289 <cvargas-2019289@kinal.edu.gt>
2021-09-27 14:47:11 -0600

    diseño de inicio y diseño de comentario, like y vistas

--- src/post/models.py
+++ src/post/models.py
@@ -28,6 +28,18 @@ class Post(models.Model):
             'slug': self.slug
         })
 
+    @property
+    def get_comment_count(self):
+        return self.comment_set.all().count()
+
+    @property
+    def get_view_count(self):
+        return self.postview_set.all().count()
+
+    @property
+    def get_like_count(self):
+        return self.like_set.all().count()
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
